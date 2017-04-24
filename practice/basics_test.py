492bfee5ec4ac443cecc0b3921fc41437f73a354
Rifatul Islam Shovon <rifatul.islam@therapservices.net>
2017-04-24 15:30:20 +0600

    IO practice

--- practice/basics_test.py
+++ practice/basics_test.py
@@ -79,3 +79,4 @@ print(spam.strip('Spam'))
 
 pyperclip.paste()
 pyperclip.copy('This will copied to the system clipboard')
+
---
+++ practice/io_practice.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import os
+import pprint
+import shelve
+
+_author_ = "rifatul.islam"
+
+current_dir = os.getcwd()
+print current_dir
+
+# Making dir for resource
+# os.makedirs('./../resources')  # Comment out for further recreation of the dir
+
+print "Current dir: " + os.path.abspath(current_dir)
+print os.listdir(current_dir)
+
+# Shelve to store data on binary file
+
+shelve_file = shelve.open("test_shelve")
+cats = ['Zophie', 'Pooka', 'Simon']
+shelve_file['cats'] = cats
+shelve_file.close()
+
+sheve_file2 = shelve.open("test_shelve")
+type(sheve_file2)
+print "The popular cats are: ", sheve_file2['cats']
+
+# Pretty print
+cats = [{'name': 'Zophie', 'descption': 'chubby'}, {'name': 'Pooka', 'descption': 'fluffy'}]
+print pprint.pformat(cats)
+
