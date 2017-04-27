ae7a9f78a56c7d82cede3fc3664c63c4f27d725a
Rifatul Islam Shovon <rifatul.islam@therapservices.net>
2017-04-27 11:13:37 +0600

    Fix functions for python 3

--- practice/io_practice.py
+++ practice/io_practice.py
@@ -6,13 +6,13 @@ import shelve
 _author_ = "rifatul.islam"
 
 current_dir = os.getcwd()
-print current_dir
+print(current_dir)
 
 # Making dir for resource
 # os.makedirs('./../resources')  # Comment out for further recreation of the dir
 
-print "Current dir: " + os.path.abspath(current_dir)
-print os.listdir(current_dir)
+print("Current dir: " + os.path.abspath(current_dir))
+print(os.listdir(current_dir))
 
 # Shelve to store data on binary file
 
@@ -23,9 +23,8 @@ shelve_file.close()
 
 sheve_file2 = shelve.open("test_shelve")
 type(sheve_file2)
-print "The popular cats are: ", sheve_file2['cats']
+print("The popular cats are: ", sheve_file2['cats'])
 
 # Pretty print
 cats = [{'name': 'Zophie', 'descption': 'chubby'}, {'name': 'Pooka', 'descption': 'fluffy'}]
-print pprint.pformat(cats)
-
+print(pprint.pformat(cats))
