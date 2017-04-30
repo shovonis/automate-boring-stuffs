7393c620261f908832ee1a74c307bcb51e1d4d0b
Rifatul Islam Shovon <rifatul.islam@therapservices.net>
2017-04-30 12:55:15 +0600

    Logging in Python

---
+++ practice/logging_python.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import logging
+
+_author_ = "rifatul.islam"
+
+logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
+logging.debug("Starting the factorial Program")
+
+
+def factorial(n):
+    logging.debug("Factorial of the %s" % n)
+    total = 1
+    for i in range(1, n + 1):
+        total *= i
+        logging.debug('i is ' + str(i) + ', total is ' + str(total))
+    logging.debug('End of factorial(%s)' % n)
+    return total
+
+
+print(factorial(5))
+logging.debug('End of program')
