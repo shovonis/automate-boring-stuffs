312b504479113b6ec9a1b4cac58f4ba59712f9ce
Rifatul Islam Shovon <rifatul.islam@therapservices.net>
2017-04-30 11:13:07 +0600

    File copier

--- projects/file_copier.py
+++ projects/file_copier.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
 import shutil
+import send2trash
 import os
 
 _author_ = "rifatul.islam"
 
+os.makedirs("tmp", exist_ok=True)
+
+for fileName in os.listdir():
+    print(fileName)
+    if fileName.endswith('.txt'):
+        os.unlink(fileName)
+
+send2trash.send2trash("tmp/test.txt")
